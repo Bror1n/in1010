b5e0d64ddd27a503436a892ade15744ce01abe82
Bror1n <123093138+Bror1n@users.noreply.github.com>
2025-04-01 15:09:23 +0200

    Enhance Lesetråd and Monitor classes with file handling and concurrency support

--- innleveringer/innlevering5/Monitor.java
+++ innleveringer/innlevering5/Monitor.java
@@ -1,5 +1,13 @@
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
+
+
+
 public class Monitor {
     private Subsekvensregister subregister;
+    private ReentrantLock tabellLock = new ReentrantLock();
+    private Condition merEnnTo = tabellLock.newCondition();
+
     
     public Monitor(){
         subregister = new Subsekvensregister();
@@ -7,6 +15,7 @@ public class Monitor {
 
     public void settInn(Frekvenstabell f){
         subregister.settInn(f);
+        merEnnTo.signalAll();
     }
 
     public Frekvenstabell taUt(){
@@ -19,5 +28,25 @@ public class Monitor {
 
     public static Frekvenstabell les(String filnavn){
         return Subsekvensregister.les(filnavn);
+    }
+
+    public Frekvenstabell[] taUtTo(){
+        this.tabellLock.lock();
+        try {
+            while (this.antall() < 2){
+                merEnnTo.await();
+            }
+            Frekvenstabell[] frekvenstabell = new Frekvenstabell[2];
+            frekvenstabell[0] = this.taUt();
+            frekvenstabell[1] = this.taUt();
+            return frekvenstabell;
+        }
+        catch (InterruptedException e){
+            System.out.println("Thread was interrupted");
+            return null;
+        }
+        finally {
+            tabellLock.unlock();
+        }
     }
 }
